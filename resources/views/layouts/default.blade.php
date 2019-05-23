a8e773eff5284d822e45f7c8a54d7ff957437eaa
Frankie <Frankie.Zeng@feisu.com>
2019-05-23 06:41:29 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,12 @@
 <html>
   <head>
     <title> @yield('title','Sample')-- Laravel 新手入门教程</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
     @include('layouts._header')
     <div class="container">
+    @include('shared._messages')
     @yield('content')
     @include('layouts._footer')
     </div>
